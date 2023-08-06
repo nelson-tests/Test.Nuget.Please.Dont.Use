d6faa4e96186c180f194613480008dfd5565aabc
Nelson Nobre <technobre@gmail.com>
2023-08-06 02:04:24 +0100

    feat: new method `YourTestMethod`

--- src/MyTestClass.cs
+++ src/MyTestClass.cs
@@ -3,5 +3,6 @@
     public static class MyTestClass
     {
         public static string MyTestMethod(int year) => $"Hello World. Year: {year}";
+        public static string YourTestMethod(string name) => $"Hello {name}.";
     }
 }
