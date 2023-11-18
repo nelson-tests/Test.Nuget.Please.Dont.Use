1d5bdd8befb3364c181d5c4b9dcf26f667c52e99
Nelson Nobre <technobre@gmail.com>
2023-11-18 18:50:16 +0000

    test: test

--- src/MyTestClass.cs
+++ src/MyTestClass.cs
@@ -8,6 +8,7 @@
         public static string WeTestMethod(string we) => $"We are {we}.";
 
         public static int Sum(int a, int b) => a + b;
+        public static int Sum(int a, int b, int d) => a + b + d;
 
         public static int Sub(int a, int b) => a - b;
 
